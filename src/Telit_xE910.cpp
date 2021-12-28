edc72054554693160af937ff53113e2796a612f0
Mehmet Günce Akkoyun <akkoyun@me.com>
2021-12-28 16:15:26 +0300

    01.00.12 - TXMONMODE Function added.

--- src/Telit_xE910.cpp
+++ src/Telit_xE910.cpp
@@ -1017,6 +1017,81 @@ bool xE910_AT::SLED(const uint8_t _SLED) {
     }
 
 }
+bool xE910_AT::TXMONMODE(const uint8_t _TXMONMODE) {
+
+	// Command Chain Delay (Advice by Telit)
+	delay(10);
+
+    // Declare Response Length
+    uint8_t _Response_Length = 6;
+
+	// Set Control Variable
+	Command_Control.TXMONMODE = false;
+
+	// Clear UART Buffer
+    _Clear_UART_Buffer();
+
+	// Send UART Command
+	GSM_Serial.print(F("AT#TXMONMODE="));
+	GSM_Serial.print(String(_TXMONMODE));
+	GSM_Serial.print(F("\r\n"));
+
+	// Wait for UART Data Send
+	GSM_Serial.flush();
+
+	// Handle Response
+	if (_Response_Wait(_Response_Length, 500)) {
+
+		// Declare Read Order Variable
+		uint8_t _Read_Order = 0;
+
+		// Declare Response Variable
+		char _Response[_Response_Length];
+
+		// Read UART Response
+		while (GSM_Serial.available() > 0) {
+
+			// Read Serial Char
+			_Response[_Read_Order] = GSM_Serial.read();
+
+			// Increase Read Order
+			_Read_Order++;
+
+			// Stream Delay
+			delayMicroseconds(500);
+
+		}
+
+		// Control for Response
+		if (strstr(_Response, "OK") != NULL) {
+
+			// Set Control Variable
+			Command_Control.TXMONMODE = true;
+
+			// End Function
+			return (true);
+
+		} else {
+
+			// Set Control Variable
+			Command_Control.TXMONMODE = false;
+
+			// End Function
+			return (false);
+
+		}
+
+    } else {
+
+		// Set Control Variable
+		Command_Control.TXMONMODE = false;
+
+		// End Function
+		return (false);
+
+    }
+
+}
 
 
 
