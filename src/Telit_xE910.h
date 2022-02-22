891fb723bcfdbfaf0f0d3088d54f78e1fbfb4483
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-02-22 17:23:47 +0300

    01.10.08 - Update

--- src/Telit_xE910.h
+++ src/Telit_xE910.h
@@ -26,7 +26,7 @@ class xE910_GSM {
 		/**
 		* Library global variables declarations.
 		*/
-		const char 	Version[9] 					= "01.09.06";		// Library Version
+		const char 	Version[9] 					= "01.10.08";		// Library Version
 		bool		Debug_Mode					= false;			// Debug Mode Variable
 
 		/**
